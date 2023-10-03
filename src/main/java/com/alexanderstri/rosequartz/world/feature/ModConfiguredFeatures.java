e386343974f28a66683583a026a97e22bb1933ac
alexanderGenesis <137672254+alexanderGenesis@users.noreply.github.com>
2023-10-03 16:42:21 +1100

    Ore spawning works.

--- src/main/java/com/alexanderstri/rosequartz/world/feature/ModConfiguredFeatures.java
+++ src/main/java/com/alexanderstri/rosequartz/world/feature/ModConfiguredFeatures.java
@@ -3,6 +3,7 @@ package com.alexanderstri.rosequartz.world.feature;
 import com.alexanderstri.rosequartz.RoseQuartz;
 import com.alexanderstri.rosequartz.block.ModBlocks;
 import com.google.common.base.Suppliers;
+import java.util.function.Supplier;
 import net.minecraft.core.Registry;
 import net.minecraft.data.worldgen.features.OreFeatures;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
@@ -13,7 +14,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 
 import java.util.List;
-import java.util.function.Supplier;
+
 
 public class ModConfiguredFeatures {
     public static final DeferredRegister<ConfiguredFeature<?, ?>> CONFIGURED_FEATURES =
@@ -24,7 +25,7 @@ public class ModConfiguredFeatures {
             OreConfiguration.target(OreFeatures.DEEPSLATE_ORE_REPLACEABLES, ModBlocks.DEEPSLATE_ROSE_QUARTZ_ORE.get().defaultBlockState())));
 
     public static final RegistryObject<ConfiguredFeature<?, ?>> ROSE_QUARTZ_ORE = CONFIGURED_FEATURES.register("rose_quartz_ore",
-            () -> new ConfiguredFeature<>(Feature.ORE, new OreConfiguration(OVERWORLD_ROSE_QUARTZ_ORE.get(), 12)));
+            () -> new ConfiguredFeature<>(Feature.ORE, new OreConfiguration(OVERWORLD_ROSE_QUARTZ_ORE.get(), 9)));
 
     public static void register(IEventBus eventBus) {
         CONFIGURED_FEATURES.register(eventBus);
